1fb7d31609a1ee60b0125d62288f70c1d7c1ea56
nep-xion <91066868+nep-xion@users.noreply.github.com>
2020-06-07 19:44:59 +0000

    kswapd: assign new_order and new_classzone_idx after wakeup in sleeping

--- src/main/java/MaxFeeTxHandler.java
+++ src/main/java/MaxFeeTxHandler.java
@@ -1,2 +1,44 @@
 
 import java.util.Set;
+import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class MaxFeeTxHandler {
+    private UTXOPool pool;
+
+    /**
+     * Creates a public ledger whose current UTXOPool (collection of unspent transaction outputs) is
+     * {@code utxoPool}. This should make a copy of utxoPool by using the UTXOPool(UTXOPool uPool)
+     * constructor.
+     */
+    public MaxFeeTxHandler(UTXOPool utxoPool) {
+        pool = new UTXOPool(utxoPool);
+    }
+
+    /**
+     * @return true if:
+     * (1) all outputs claimed by {@code tx} are in the current UTXO pool,
+     * (2) the signatures on each input of {@code tx} are valid,
+     * (3) no UTXO is claimed multiple times by {@code tx},
+     * (4) all of {@code tx}s output values are non-negative, and
+     * (5) the sum of {@code tx}s input values is greater than or equal to the sum of its output
+     *     values; and false otherwise.
+     */
+    public boolean isValidTx(Transaction tx) {
+        // Check if all outputs claimed by {@code tx} are in the current UTXO pool
+        for (int i = 0; i < tx.numInputs(); ++i) {
+            Transaction.Input in = tx.getInput(i);
+            UTXO utxo = new UTXO(in.prevTxHash, in.outputIndex);
+            if (!pool.contains(utxo)) {
+                return false;
+            }
+        }
+
+        // Check if the signatures on each input of {@code tx} are valid
+        for (int i = 0; i < tx.numInputs(); ++i) {
+            byte[] data = tx.getRawDataToSign(i);
+            Transaction.Input in = tx.getInput(i);
+            if (in.signature == null) {
+                return false;
