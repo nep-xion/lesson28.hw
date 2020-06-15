25eed327ff7bd993c9d944ea119510e958f38a12
nep-xion <91066868+nep-xion@users.noreply.github.com>
2020-06-15 14:04:46 +0000

    Improve parsing

--- src/main/java/Transaction.java
+++ src/main/java/Transaction.java
@@ -21,3 +21,53 @@ public class Transaction {
                 prevTxHash = null;
             else
                 prevTxHash = Arrays.copyOf(prevHash, prevHash.length);
+            outputIndex = index;
+        }
+
+        public void addSignature(byte[] sig) {
+            if (sig == null)
+                signature = null;
+            else
+                signature = Arrays.copyOf(sig, sig.length);
+        }
+    }
+
+    public class Output {
+        /** value in bitcoins of the output */
+        public double value;
+        /** the address or public key of the recipient */
+        public PublicKey address;
+
+        public Output(double v, PublicKey addr) {
+            value = v;
+            address = addr;
+        }
+    }
+
+    /** hash of the transaction, its unique id */
+    private byte[] hash;
+    private ArrayList<Input> inputs;
+    private ArrayList<Output> outputs;
+
+    public Transaction() {
+        inputs = new ArrayList<Input>();
+        outputs = new ArrayList<Output>();
+    }
+
+    public Transaction(Transaction tx) {
+        hash = tx.hash.clone();
+        inputs = new ArrayList<Input>(tx.inputs);
+        outputs = new ArrayList<Output>(tx.outputs);
+    }
+
+    public void addInput(byte[] prevTxHash, int outputIndex) {
+        Input in = new Input(prevTxHash, outputIndex);
+        inputs.add(in);
+    }
+
+    public void addOutput(double value, PublicKey address) {
+        Output op = new Output(value, address);
+        outputs.add(op);
+    }
+
+    public void removeInput(int index) {
