25a21d50649ddda9042297b7aa83123f69b4f837
nep-xion <91066868+nep-xion@users.noreply.github.com>
2020-08-14 21:37:09 +0000

    Выпуск

--- src/main/java/TxHandler.java
+++ src/main/java/TxHandler.java
@@ -19,4 +19,5 @@ public class TxHandler {
      * (1) all outputs claimed by {@code tx} are in the current UTXO pool,
      * (2) the signatures on each input of {@code tx} are valid,
      * (3) no UTXO is claimed multiple times by {@code tx},
-     * (4) all of {@code 
+     * (4) all of {@code tx}s output values are non-negative, and
+     * (5
