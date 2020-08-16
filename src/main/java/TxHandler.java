19886a7824906e6697752db345138e9a02ec1b11
nep-xion <91066868+nep-xion@users.noreply.github.com>
2020-08-16 21:22:13 +0000

    More messages

--- src/main/java/TxHandler.java
+++ src/main/java/TxHandler.java
@@ -20,4 +20,5 @@ public class TxHandler {
      * (2) the signatures on each input of {@code tx} are valid,
      * (3) no UTXO is claimed multiple times by {@code tx},
      * (4) all of {@code tx}s output values are non-negative, and
-     * (5) the sum of {@code tx}s input values is grea
+     * (5) the sum of {@code tx}s input values is greater than or equal to the sum of its output
+     *     
