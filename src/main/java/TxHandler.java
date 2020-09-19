69add09efa5950d2667021400bdc34eafd54a890
nep-xion <91066868+nep-xion@users.noreply.github.com>
2020-09-19 09:16:37 +0000

    Added missing tests

--- src/main/java/TxHandler.java
+++ src/main/java/TxHandler.java
@@ -28,4 +28,5 @@ public class TxHandler {
         for (int i = 0; i < tx.numInputs(); ++i) {
             Transaction.Input in = tx.getInput(i);
             UTXO utxo = new UTXO(in.prevTxHash, in.outputIndex);
-      
+            if (!pool.contains(utxo)) {
+                r
