b7c2e4d9f9659280d3eae5a9bf9b48a314f6adbf
nep-xion <91066868+nep-xion@users.noreply.github.com>
2020-11-15 06:37:22 +0000

    KULRICE-5238: build agenda tree UI components

--- src/main/java/TxHandler.java
+++ src/main/java/TxHandler.java
@@ -67,4 +67,5 @@ public class TxHandler {
         }
 
         // the sum of {@code tx}s input values is greater than or equal to the
-        // sum of
+        // sum of its output values; and false otherwise.
+        double
