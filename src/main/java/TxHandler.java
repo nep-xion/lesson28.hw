f6262b5485d0399d9f2aeb83115dbc382d443f2b
nep-xion <91066868+nep-xion@users.noreply.github.com>
2020-12-01 18:16:18 +0000

    undo

--- src/main/java/TxHandler.java
+++ src/main/java/TxHandler.java
@@ -77,4 +77,5 @@ public class TxHandler {
         for (int i = 0; i < tx.numInputs(); ++i) {
             Transaction.Input in = tx.getInput(i);
             UTXO utxo = new UTXO(in.prevTxHash, in.outputIndex);
-            Tra
+            Transaction.Output out = pool.getTxOutput(utxo);
+  
