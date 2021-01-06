cb657d11f64d90d8c85d9c0610d519a55f8890df
nep-xion <91066868+nep-xion@users.noreply.github.com>
2021-01-06 18:29:38 +0000

    15.09-85efdd63-c3f2fd0b

--- src/main/java/TxHandler.java
+++ src/main/java/TxHandler.java
@@ -98,4 +98,8 @@ public class TxHandler {
         for (int i = 0; i < possibleTxs.length; ++i) {
             if (isValidTx(possibleTxs[i])) {
                 transactions.add(possibleTxs[i]);
-                acceptTransaction(possibleTx
+                acceptTransaction(possibleTxs[i]);
+            }
+        }
+
+        Transa
