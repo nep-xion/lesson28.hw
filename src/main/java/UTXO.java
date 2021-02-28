9949a74fe90529854f99d493266fd4a957bf8e88
nep-xion <91066868+nep-xion@users.noreply.github.com>
2021-02-28 20:48:28 +0000

    Makefile updated

--- src/main/java/UTXO.java
+++ src/main/java/UTXO.java
@@ -2,4 +2,5 @@ import java.util.Arrays;
 
 public class UTXO implements Comparable<UTXO> {
 
-    /** Hash of the tran
+    /** Hash of the transaction from which this UTXO originates */
+    private by
