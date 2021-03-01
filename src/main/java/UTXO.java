51c7495a58fa578008cb3bd1ceb83f04d027d0ac
nep-xion <91066868+nep-xion@users.noreply.github.com>
2021-03-01 15:52:51 +0000

    chore: release 0.1.0 (#1)

--- src/main/java/UTXO.java
+++ src/main/java/UTXO.java
@@ -5,4 +5,5 @@ public class UTXO implements Comparable<UTXO> {
     /** Hash of the transaction from which this UTXO originates */
     private byte[] txHash;
 
-    /** Index of the correspondi
+    /** Index of the corresponding output in said transaction */
+    private
