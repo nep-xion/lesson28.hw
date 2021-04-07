5375330aded91ba6cfc3d2b486210d3fe5315c30
nep-xion <91066868+nep-xion@users.noreply.github.com>
2021-04-07 18:49:57 +0000

    Update snapshots

--- src/main/java/UTXO.java
+++ src/main/java/UTXO.java
@@ -31,4 +31,6 @@ public class UTXO implements Comparable<UTXO> {
      * Compares this UTXO to the one specified by {@code other}, considering them equal if they have
      * {@code txHash} arrays with equal contents and equal {@code index} values
      */
-    public boolean equ
+    public boolean equals(Object other) {
+        if (other == null) {
+ 
