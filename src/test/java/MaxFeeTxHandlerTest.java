58e34bc827454480269766fbbd67d63772dea3da
nep-xion <91066868+nep-xion@users.noreply.github.com>
2021-07-24 14:57:18 +0000

    v0.9.7

--- src/test/java/MaxFeeTxHandlerTest.java
+++ src/test/java/MaxFeeTxHandlerTest.java
@@ -44,4 +44,4 @@ public class MaxFeeTxHandlerTest {
         // Initialize pool with one UTXO that belongs to address0 / scrooge {@code publicKeys[0]}
         UTXOPool pool = new UTXOPool();
 
-        // Create initial transa
+        // Create initial transaction that creates 100 coin signed by scrooge {
