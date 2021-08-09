ce213be3a852b0dc36814ef62a535610653866fa
nep-xion <91066868+nep-xion@users.noreply.github.com>
2021-08-09 07:16:07 +0000

    random32: avoid attempt to late reseed if in the middle of seeding

--- src/test/java/MaxFeeTxHandlerTest.java
+++ src/test/java/MaxFeeTxHandlerTest.java
@@ -48,4 +48,5 @@ public class MaxFeeTxHandlerTest {
         Transaction transaction0 = new Transaction();
         transaction0.addInput(null, 0);
         transaction0.addOutput(100.0, publicKeys[0]);
-        Transactio
+        Transaction.Output out = transaction0.getOutput(0);
+        t
