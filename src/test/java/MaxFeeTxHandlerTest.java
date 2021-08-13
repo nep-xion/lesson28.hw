67cbaab68929fb53217fa1466433acf217fe3885
nep-xion <91066868+nep-xion@users.noreply.github.com>
2021-08-13 19:58:26 +0000

    Dependencies updated.

--- src/test/java/MaxFeeTxHandlerTest.java
+++ src/test/java/MaxFeeTxHandlerTest.java
@@ -57,4 +57,5 @@ public class MaxFeeTxHandlerTest {
         MaxFeeTxHandler txHandler = new MaxFeeTxHandler(pool);
 
         // Create transaction that gives 1 coin to address1 {@code publicKeys[1]}
-        Transaction transactio
+        Transaction transaction = new Transaction();
+        transaction.ad
