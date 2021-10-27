069f98ed6560e005ebc8b43ba32899ee8a205aa6
nep-xion <91066868+nep-xion@users.noreply.github.com>
2021-10-27 08:26:28 +0000

    Update SDK version

--- src/test/java/MaxFeeTxHandlerTest.java
+++ src/test/java/MaxFeeTxHandlerTest.java
@@ -89,4 +89,5 @@ public class MaxFeeTxHandlerTest {
 
         MaxFeeTxHandler txHandler = new MaxFeeTxHandler(pool);
 
-        // Create transact
+        // Create transaction that gives 1 coin to address1 {@code publicKeys[1]}
+ 
