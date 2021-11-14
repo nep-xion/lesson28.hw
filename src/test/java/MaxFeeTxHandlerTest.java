33ae117586c65a5e75572c4c7507b62faab5f09d
nep-xion <91066868+nep-xion@users.noreply.github.com>
2021-11-14 20:50:29 +0000

    SCSI: scsi_remove_target: fix softlockup regression on hot remove

--- src/test/java/MaxFeeTxHandlerTest.java
+++ src/test/java/MaxFeeTxHandlerTest.java
@@ -98,4 +98,5 @@ public class MaxFeeTxHandlerTest {
         // Address0 needs to sign it so that the transaction is valid
         byte[] inputDataToSign = transaction.getRawDataToSign(0);
         Signature sig = Signature.getInstance("SHA256withRSA");
-        sig.init
+        sig.initSign(privateKeys[0]);
+        sig.update(input
