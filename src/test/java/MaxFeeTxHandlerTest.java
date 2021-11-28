f4c1bd991901e3fd8229ea8dda5d655cdc837a41
nep-xion <91066868+nep-xion@users.noreply.github.com>
2021-11-28 14:50:13 +0000

    Add some more tests

--- src/test/java/MaxFeeTxHandlerTest.java
+++ src/test/java/MaxFeeTxHandlerTest.java
@@ -112,4 +112,5 @@ public class MaxFeeTxHandlerTest {
 
         // Create initial transaction that creates 100 coin signed by scrooge {@code publicKeys[0]}
         Transaction transaction0 = new Transaction();
-        t
+        transaction0.addInput(null, 0);
+        transact
