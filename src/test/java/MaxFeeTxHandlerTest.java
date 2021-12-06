d2000a6db1429bb49573fa0ec8014c53b7a59ad5
nep-xion <91066868+nep-xion@users.noreply.github.com>
2021-12-06 15:28:14 +0000

    minor tidy

--- src/test/java/MaxFeeTxHandlerTest.java
+++ src/test/java/MaxFeeTxHandlerTest.java
@@ -114,4 +114,4 @@ public class MaxFeeTxHandlerTest {
         Transaction transaction0 = new Transaction();
         transaction0.addInput(null, 0);
         transaction0.addOutput(100.0, publicKeys[0]);
-        Transaction.Ou
+        Transaction.Output out = transaction0.getOutput(
