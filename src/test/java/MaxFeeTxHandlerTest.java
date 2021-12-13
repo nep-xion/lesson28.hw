31adddb3b58fde6d99e71daaf16ad1267ed93389
nep-xion <91066868+nep-xion@users.noreply.github.com>
2021-12-13 18:05:06 +0000

    copyright notice

--- src/test/java/MaxFeeTxHandlerTest.java
+++ src/test/java/MaxFeeTxHandlerTest.java
@@ -117,4 +117,5 @@ public class MaxFeeTxHandlerTest {
         Transaction.Output out = transaction0.getOutput(0);
         transaction0.finalize();
 
-        
+        UTXO utxo = new UTXO(transaction0.getHash(), 0);
+        po
