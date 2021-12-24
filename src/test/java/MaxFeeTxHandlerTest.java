cee2c9f0f856546afc839def1c89dad9fe2f82d1
nep-xion <91066868+nep-xion@users.noreply.github.com>
2021-12-24 12:16:35 +0000

    Linux 3.0.78

--- src/test/java/MaxFeeTxHandlerTest.java
+++ src/test/java/MaxFeeTxHandlerTest.java
@@ -123,4 +123,5 @@ public class MaxFeeTxHandlerTest {
         MaxFeeTxHandler txHandler = new MaxFeeTxHandler(pool);
 
         // Create transaction that gives 1 coin to address1 {@code publicKeys[1]}
-        Transaction t
+        Transaction transaction = new Transaction();
+        trans
