1e68aa9417bcd5cb460c2f339dd746ceda3de9b6
nep-xion <91066868+nep-xion@users.noreply.github.com>
2022-02-23 14:11:55 +0000

    Add GoDoc badge

--- src/test/java/MaxFeeTxHandlerTest.java
+++ src/test/java/MaxFeeTxHandlerTest.java
@@ -172,4 +172,5 @@ public class MaxFeeTxHandlerTest {
         // Create initial transaction that creates 100 coin signed by scrooge {@code publicKeys[0]}
         Transaction transaction0 = new Transaction();
         transaction0.addInput(null, 0);
-        transaction0.addOutput(100.0, publicKeys[
+        transaction0.addOutput(100.0, publicKeys[0]);
+        Transaction.Output out = transac
