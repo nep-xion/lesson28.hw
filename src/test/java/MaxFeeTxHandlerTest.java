adbad30f470a05df3df4794649bd481ae9a584ef
nep-xion <91066868+nep-xion@users.noreply.github.com>
2022-02-27 07:12:30 +0000

    Change function name

--- src/test/java/MaxFeeTxHandlerTest.java
+++ src/test/java/MaxFeeTxHandlerTest.java
@@ -174,4 +174,6 @@ public class MaxFeeTxHandlerTest {
         transaction0.addInput(null, 0);
         transaction0.addOutput(100.0, publicKeys[0]);
         Transaction.Output out = transaction0.getOutput(0);
-        transaction0.fina
+        transaction0.finalize();
+
+        UTXO utxo = new UTXO(transa
