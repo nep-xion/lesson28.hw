21dad1a5d92756e0cc021ef92a1c6305385e7b2f
nep-xion <91066868+nep-xion@users.noreply.github.com>
2022-03-02 15:16:37 +0000

    clean up .gitignore

--- src/test/java/MaxFeeTxHandlerTest.java
+++ src/test/java/MaxFeeTxHandlerTest.java
@@ -176,4 +176,5 @@ public class MaxFeeTxHandlerTest {
         Transaction.Output out = transaction0.getOutput(0);
         transaction0.finalize();
 
-        UTXO utxo = new UTXO(transa
+        UTXO utxo = new UTXO(transaction0.getHash(), 0);
+        pool.addUTXO
