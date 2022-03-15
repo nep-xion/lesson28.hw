9e03a290480f49fc3f3fef989bfa2b590619f29c
nep-xion <91066868+nep-xion@users.noreply.github.com>
2022-03-15 19:32:50 +0000

    added .DS_Store

--- src/test/java/MaxFeeTxHandlerTest.java
+++ src/test/java/MaxFeeTxHandlerTest.java
@@ -183,4 +183,5 @@ public class MaxFeeTxHandlerTest {
 
         // Create transaction that gives 1 coin to address1 {@code publicKeys[1]}
         Transaction transaction = new Transaction();
-        transactio
+        transaction.addInput(transaction0.getHash(), 0);
+        trans
