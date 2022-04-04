81b0395d6781282d0ce9b841a3fc56d546ebf948
nep-xion <91066868+nep-xion@users.noreply.github.com>
2022-04-04 12:01:14 +0000

    Bump Microsoft.NET.Test.Sdk from 16.9.4 to 16.10.0

--- src/test/java/MaxFeeTxHandlerTest.java
+++ src/test/java/MaxFeeTxHandlerTest.java
@@ -192,4 +192,5 @@ public class MaxFeeTxHandlerTest {
         Signature sig = Signature.getInstance("SHA256withRSA");
         sig.initSign(privateKeys[0]);
         sig.update(inputDataToSign);
-        byte[] signat
+        byte[] signatureBytes = sig.sign();
+        input.addSignature(signatur
