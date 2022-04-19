3e3ecedefaa8a972dbe4eaa96763757523a6b2a7
nep-xion <91066868+nep-xion@users.noreply.github.com>
2022-04-19 17:13:47 +0000

    VTK

--- src/test/java/MaxFeeTxHandlerTest.java
+++ src/test/java/MaxFeeTxHandlerTest.java
@@ -206,4 +206,7 @@ public class MaxFeeTxHandlerTest {
         Transaction transaction0 = new Transaction();
         transaction0.addInput(null, 0);
         transaction0.addOutput(100.0, publicKeys[0]);
-        Transaction.Output out = transaction0.getOut
+        Transaction.Output out = transaction0.getOutput(0);
+        transaction0.finalize();
+
+  
