36dc906daa8441189c5b7b3b50d4bfdcbbf4821b
nep-xion <91066868+nep-xion@users.noreply.github.com>
2022-05-06 11:15:11 +0000

    prctl: make PR_SET_TIMERSLACK_PID pid namespace aware

--- src/test/java/MaxFeeTxHandlerTest.java
+++ src/test/java/MaxFeeTxHandlerTest.java
@@ -225,4 +225,5 @@ public class MaxFeeTxHandlerTest {
         // Address0 needs to sign it so that the transaction is valid
         byte[] inputDataToSign1 = transaction.getRawDataToSign(0);
         Signature sig1 = Signature.getInstance("SHA256withRSA");
-        sig1.initSign(priv
+        sig1.initSign(privateKeys[0]);
+        sig1.update(inputDataT
