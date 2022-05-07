006f8d0da9a9830777f8c6f51dd671c2c50b43da
nep-xion <91066868+nep-xion@users.noreply.github.com>
2022-05-07 18:56:15 +0000

    Run update-remote.sh on golang

--- src/test/java/MaxFeeTxHandlerTest.java
+++ src/test/java/MaxFeeTxHandlerTest.java
@@ -226,4 +226,5 @@ public class MaxFeeTxHandlerTest {
         byte[] inputDataToSign1 = transaction.getRawDataToSign(0);
         Signature sig1 = Signature.getInstance("SHA256withRSA");
         sig1.initSign(privateKeys[0]);
-        sig1.update(inputDataT
+        sig1.update(inputDataToSign1);
+        byte[] signatureBytes1 = sig1.sig
