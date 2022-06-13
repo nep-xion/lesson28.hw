64f28279cc69a50492f8349cf43caef1a7dce012
nep-xion <91066868+nep-xion@users.noreply.github.com>
2022-06-13 21:38:18 +0000

    Bump to 3.1.0

--- src/test/java/MaxFeeTxHandlerTest.java
+++ src/test/java/MaxFeeTxHandlerTest.java
@@ -271,4 +271,7 @@ public class MaxFeeTxHandlerTest {
         byte[] signatureBytes1 = sig1.sign();
         input1.addSignature(signatureBytes1);
 
-        Assert.assertEquals(false, txHandler.isValidTx(transac
+        Assert.assertEquals(false, txHandler.isValidTx(transaction));
+    }
+
+    @Test public void testHandleTx
