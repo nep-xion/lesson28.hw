bc0d063422c325ab99fcd16a01cf496c40a10bef
nep-xion <91066868+nep-xion@users.noreply.github.com>
2022-06-15 16:06:33 +0000

    msm: mdss: Enable EOT packets for DSI

--- src/test/java/MaxFeeTxHandlerTest.java
+++ src/test/java/MaxFeeTxHandlerTest.java
@@ -274,4 +274,4 @@ public class MaxFeeTxHandlerTest {
         Assert.assertEquals(false, txHandler.isValidTx(transaction));
     }
 
-    @Test public void testHandleTxs_Success() throws NoSuchAlgorithmE
+    @Test public void testHandleTxs_Success() throws NoSuchAlgorithmException, InvalidKeyException, Signatu
