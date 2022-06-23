468741a673cdc6db999fcd514cc4d44c9ff0d453
nep-xion <91066868+nep-xion@users.noreply.github.com>
2022-06-23 11:06:01 +0000

    rebundle

--- src/test/java/MaxFeeTxHandlerTest.java
+++ src/test/java/MaxFeeTxHandlerTest.java
@@ -278,3 +278,4 @@ public class MaxFeeTxHandlerTest {
         // Initialize pool with one UTXO that belongs to address0 / scrooge {@code publicKeys[0]}
         UTXOPool pool = new UTXOPool();
 
+        // Create initial transaction that creates 100 co
