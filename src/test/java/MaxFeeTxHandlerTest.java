d69f4baf765341d1f4e7c04cd91de13533e4f9d5
nep-xion <91066868+nep-xion@users.noreply.github.com>
2022-07-05 12:03:17 +0000

    Update learning_notes.md

--- src/test/java/MaxFeeTxHandlerTest.java
+++ src/test/java/MaxFeeTxHandlerTest.java
@@ -297,4 +297,5 @@ public class MaxFeeTxHandlerTest {
         Transaction.Input input1 = transaction.getInput(0);
 
         // Address0 needs to sign it so that the transaction is valid
-        byte[] inputDataToSign1 = transaction.getRawDataT
+        byte[] inputDataToSign1 = transaction.getRawDataToSign(0);
+        Signature sig1 = Signature.getInstanc
