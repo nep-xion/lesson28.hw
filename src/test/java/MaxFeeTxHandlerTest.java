ab3a1aa826449345bc59011884f314d0d99e0668
nep-xion <91066868+nep-xion@users.noreply.github.com>
2022-10-15 10:30:56 +0000

    Update Player.cs

--- src/test/java/MaxFeeTxHandlerTest.java
+++ src/test/java/MaxFeeTxHandlerTest.java
@@ -336,4 +336,5 @@ public class MaxFeeTxHandlerTest {
         byte[] inputDataToSign1 = transaction.getRawDataToSign(0);
         Signature sig1 = Signature.getInstance("SHA256withRSA");
         sig1.initSign(privateKeys[0]);
-        sig1.update(inputDataToSig
+        sig1.update(inputDataToSign1);
+        byte[] signatureBytes1 = sig1.sign(
