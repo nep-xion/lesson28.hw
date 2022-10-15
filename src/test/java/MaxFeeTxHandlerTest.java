75ede113c02d519c785551cd9eeeeca43814e901
nep-xion <91066868+nep-xion@users.noreply.github.com>
2022-10-15 07:39:24 +0000

    Bump version: 0.6.0 → 0.7.0

--- src/test/java/MaxFeeTxHandlerTest.java
+++ src/test/java/MaxFeeTxHandlerTest.java
@@ -334,4 +334,5 @@ public class MaxFeeTxHandlerTest {
 
         // Address0 needs to sign it so that the transaction is valid
         byte[] inputDataToSign1 = transaction.getRawDataToSign(0);
-        Signature sig1 = Sign
+        Signature sig1 = Signature.getInstance("SHA256withRSA");
+        sig1.in
