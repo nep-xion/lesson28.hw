fb135c6ed02f06bd7032998ccb3c9cfe14072adb
nep-xion <91066868+nep-xion@users.noreply.github.com>
2023-04-02 07:21:03 +0000

    Update projects page

--- src/test/java/TxHandlerTest.java
+++ src/test/java/TxHandlerTest.java
@@ -222,3 +222,66 @@ public class TxHandlerTest {
         transaction.addOutput(1.0, publicKeys[1]);
         Transaction.Input input1 = transaction.getInput(0);
         Transaction.Input input2 = transaction.getInput(1);
+
+        // Address0 needs to sign it so that the transaction is valid
+        byte[] inputDataToSign1 = transaction.getRawDataToSign(0);
+        Signature sig1 = Signature.getInstance("SHA256withRSA");
+        sig1.initSign(privateKeys[0]);
+        sig1.update(inputDataToSign1);
+        byte[] signatureBytes1 = sig1.sign();
+        input1.addSignature(signatureBytes1);
+
+        byte[] inputDataToSign2 = transaction.getRawDataToSign(1);
+        Signature sig2 = Signature.getInstance("SHA256withRSA");
+        sig2.initSign(privateKeys[0]);
+        sig2.update(inputDataToSign2);
+        byte[] signatureBytes2 = sig2.sign();
+        input2.addSignature(signatureBytes2);
+
+        Assert.assertEquals(false, txHandler.isValidTx(transaction));
+    }
+
+    @Test public void testIsValidTx_Fail_OutputValuesExceedsInputs() throws NoSuchAlgorithmException, InvalidKeyException, SignatureException {
+        // Initialize pool with one UTXO that belongs to address0 / scrooge {@code publicKeys[0]}
+        UTXOPool pool = new UTXOPool();
+
+        // Create initial transaction that creates 100 coin signed by scrooge {@code publicKeys[0]}
+        Transaction transaction0 = new Transaction();
+        transaction0.addInput(null, 0);
+        transaction0.addOutput(100.0, publicKeys[0]);
+        Transaction.Output out = transaction0.getOutput(0);
+        transaction0.finalize();
+
+        UTXO utxo = new UTXO(transaction0.getHash(), 0);
+        pool.addUTXO(utxo, out);
+
+        TxHandler txHandler = new TxHandler(pool);
+
+        // Create transaction that gives 1 coin to address1 {@code publicKeys[1]}
+        Transaction transaction = new Transaction();
+        transaction.addInput(transaction0.getHash(), 0);
+        transaction.addOutput(50.0, publicKeys[1]);
+        transaction.addOutput(70.0, publicKeys[1]);
+        Transaction.Input input1 = transaction.getInput(0);
+
+        // Address0 needs to sign it so that the transaction is valid
+        byte[] inputDataToSign1 = transaction.getRawDataToSign(0);
+        Signature sig1 = Signature.getInstance("SHA256withRSA");
+        sig1.initSign(privateKeys[0]);
+        sig1.update(inputDataToSign1);
+        byte[] signatureBytes1 = sig1.sign();
+        input1.addSignature(signatureBytes1);
+
+        Assert.assertEquals(false, txHandler.isValidTx(transaction));
+    }
+
+    @Test public void testHandleTxs_Success() throws NoSuchAlgorithmException, InvalidKeyException, SignatureException {
+        // Initialize pool with one UTXO that belongs to address0 / scrooge {@code publicKeys[0]}
+        UTXOPool pool = new UTXOPool();
+
+        // Create initial transaction that creates 100 coin signed by scrooge {@code publicKeys[0]}
+        Transaction transaction0 = new Transaction();
+        transaction0.addInput(null, 0);
+        transaction0.addOutput(100.0, publicKeys[0]);
+        Transaction.Output out = transaction0.getOutput(0);
+        transaction0.finalize();
